1dd3a76e0fbb420b8b84736c7d6c7f2f9d8f6e66
williamneo <williamneokh@gmail.com>
2022-03-28 12:03:34 +0800

    updated hello world code

--- Hello.go
+++ Hello.go
@@ -5,9 +5,9 @@ import (
 )
 
 func HelloWorld() string {
-	//Insert your code here
+
 	messageHelloWorld := "Hello World from Go !!"
-	//Do not remove this line here
+
 	return messageHelloWorld
 }
 
